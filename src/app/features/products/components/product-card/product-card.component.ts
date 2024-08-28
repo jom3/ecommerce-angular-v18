2e40b647baa0d9a6d5155371eeeab1743112bd45
joams mogro gomez <joamsmg@gmail.com>
2024-08-27 21:33:24 -0400

    feat: add to cart button implemented

--- src/app/features/products/components/product-card/product-card.component.ts
+++ src/app/features/products/components/product-card/product-card.component.ts
@@ -1,20 +1,16 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { Product } from '../../../../shared/models/product';
 import { CurrencyPipe, SlicePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { AddToCartButtonComponent } from "../add-to-cart-button/add-to-cart-button.component";
 
 @Component({
   selector: 'product-card',
   standalone: true,
-  imports: [CurrencyPipe, SlicePipe, RouterLink],
+  imports: [CurrencyPipe, SlicePipe, RouterLink, AddToCartButtonComponent],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss'
 })
 export class ProductCardComponent {
   product = input.required<Product>()
-  addToCartEvent = output<Product>()
-
-  public addToCart():void{
-    this.addToCartEvent.emit(this.product())
-  }
 }
